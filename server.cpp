c45133f991d7e5d56b06ebc2003d39da6e9560c7
ElementalAlchemist <ElementAlchemist7@gmail.com>
2009-10-16 00:59:12 -0400

    Write (most of) server class

--- server.cpp
+++ server.cpp
@@ -1,2 +1,38 @@
 #include "server.h"
 
+class Server {
+	public:
+		Server(std::tr1::unordered_map<std::string, std::string> serverConfig);
+		void parseCapab(std::vector<std::string> line005);
+		void sendMsg(std::string message);
+		bool isConnected();
+	private:
+		std::string receiveLine();
+		std::string networkName;
+		std::vector<char> statusModes, chanTypes, chanModes;
+		short numModes;
+		Socket connection ();
+};
+
+Server::Server(std::tr1::unordered_map<std::string, std::string> serverConfig) {
+	connection.connect(serverConfig["address"], serverConfig["port"]);
+	// handle the whole connection to the server soon
+}
+
+void Server::parseCapab(std::vector<std::string> line005) {
+	// I'll handle this when I get there.
+}
+
+void Server::sendMsg(std::string message) {
+	std::cout << networkName << ">" << message << std::endl;
+	message += "\r\n";
+	connection.send(message);
+}
+
+std::string Server::receiveLine() {
+	return connection.receive();
+}
+
+bool Server::isConnected() {
+	return connection.connected;
+}
