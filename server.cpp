0c3f7244c44dda063d9acdeca892b1d43ab37ff3
ElementalAlchemist <ElementAlchemist7@gmail.com>
2009-10-16 20:55:52 -0400

    Move lineParser to the server class.

--- server.cpp
+++ server.cpp
@@ -8,6 +8,7 @@ class Server {
 		void parseCapab(std::vector<std::string> line005);
 		void sendMsg(std::string message);
 		bool isConnected();
+		std::vector<std::string> parseLine(std::string message);
 	private:
 		void joinChannel(std::string channelName);
 		std::string receiveLine();
@@ -58,6 +59,25 @@ bool Server::isConnected() {
 void Server::joinChannel(std::string channelName) {
 	std::pair<std::string, Channel> oneChannel(channelName, Channel());
 	channels.insert(oneChannel);
+}
+
+std::vector<std::string> Server::parseLine(std::string message) {
+	std::string messageString;
+	std::vector<std::string> messageParams;
+	messageString = "";
+	for (unsigned int i = 0; i < message.size(); i++) {
+		if (message[i] == ' ') {
+			messageParams.push_back(messageString);
+			messageString = "";
+		} else if (message[i] == ':' && messageString == "" && i > 0) {
+			for (i++; i < message.size(); i++)
+				messageString += message[i];
+			messageParams.push_back(messageString);
+			messageString = "";
+		} else
+			messageString += message[i];
+	}
+	return messageParams;
 }
 	
 #endif
