f59096912c605afbd4aab8c0eb0c7372eac6dd22
ElementalAlchemist <ElementAlchemist7@gmail.com>
2009-11-13 12:12:58 -0500

    Get the server class done, except for interface functions and data interpretation.

--- server.cpp
+++ server.cpp
@@ -30,14 +30,17 @@ Server::Server(std::string serverAddress, std::tr1::unordered_map<std::string, s
 	handleData();
 }
 
-void joinChannel(std::string channelName) {
+void Server::joinChannel(std::string channelName) {
 	serverConnection.sendData("JOIN " + channelName);
 }
+
+// other interface functions
 
 void Server::handleData() {
 	std::string receivedLine = "";
 	while (true) {
 		receivedLine = serverConnection.receive();
+		// eventually this will interpret the data received.
 	}
 }
 
@@ -47,5 +50,45 @@ void Server::parse005(std::vector<std::string> parsedLine) {
 			serverConnection.sendData("PROTOCTL NAMESX");
 		// other parts will come soon
 	}
+}
+
+std::vector<std::string> Server::parseLine(std::string unformattedLine) {
+	std::vector<std::string> parsedLine;
+	std::string linePart = "";
+	for (unsigned int i = 0; i < unformattedLine.size(); i++) {
+		if (unformattedLine[i] == ' ') {
+			parsedLine.push_back(linePart);
+			linePart = "";
+			continue;
+		}
+		if (i > 0) {
+			if (unformattedLine[i] == ':' && unformattedLine[i-1] == ' ') {
+				for (i++; i < unformattedLine.size(); i++)
+					linePart += unformattedLine[i];
+				parsedLine.push_back(linePart);
+				break;
+			}
+		}
+		linePart += unformattedLine[i];
+	}
+	if (linePart != "")
+		parsedLine.push_back(linePart);
+	return parsedLine;
+}
+
+std::vector<std::string> Server::separateBySpace(std::string joinedLine) {
+	std::vector<std::string> words;
+	std::string wordPart;
+	for (unsigned int i = 0; i < joinedLine.size(); i++) {
+		if (joinedLine[i] == ' ') {
+			words.push_back(wordPart);
+			wordPart = "";
+			continue;
+		}
+		wordPart += joinedLine[i];
+	}
+	if (wordPart != "")
+		words.push_back(wordPart);
+	return words;
 }
 #endif
