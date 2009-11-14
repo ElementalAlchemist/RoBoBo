ba3dc2c02461fe809ec00d8ce4fcc32ba0535229
ElementalAlchemist <ElementAlchemist7@gmail.com>
2009-11-13 21:29:20 -0500

    It compiles :D

--- robobo.cpp
+++ robobo.cpp
@@ -7,7 +7,7 @@ std::list<std::string> makeServerList(ConfigReader& config) {
 	std::tr1::unordered_map<std::string, std::tr1::unordered_map<std::string, std::string> > serverConfig = config.getServerConfig();
 	std::tr1::unordered_map<std::string, std::tr1::unordered_map<std::string, std::string> >::iterator serverIterator;
 	for (serverIterator = serverConfig.begin(); serverIterator != serverConfig.end(); serverIterator++) {
-		connectedServers[serverIterator->first] = Server(serverIterator->first, serverIterator->second);
+		connectedServers.insert(std::pair<std::string, Server> (serverIterator->first, Server (serverIterator->first, serverIterator->second)));
 		serverList.insert(serverList.end(), serverIterator->first);
 	}
 	return serverList;
