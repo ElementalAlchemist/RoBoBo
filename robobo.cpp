b217d33e281e661544552351aa3cb2c924b68ec7
ElementalAlchemist <ElementAlchemist7@gmail.com>
2009-12-25 22:10:40 -0500

    Move some stuff to clean up all the passing.

--- robobo.cpp
+++ robobo.cpp
@@ -3,14 +3,16 @@
 std::tr1::unordered_map<std::string, Server*> connectedServers;
 std::tr1::unordered_map<std::string, Module*> loadedModules;
 std::list<std::string> serverList;
+ConfigReader config;
+ModuleInterface modInterface (&connectedServers, &loadedModules);
 
-inline void makeServerList(ConfigReader& config) {
+inline void makeServerList() {
 	std::tr1::unordered_map<std::string, std::tr1::unordered_map<std::string, std::string> > serverConfig = config.getServerConfig();
 	for (std::tr1::unordered_map<std::string, std::tr1::unordered_map<std::string, std::string> >::iterator serverIterator = serverConfig.begin(); serverIterator != serverConfig.end(); serverIterator++)
 		serverList.insert(serverList.end(), serverIterator->first);
 }
 
-void connectServers(ConfigReader& config, ModuleInterface& modInterface) {
+void connectServers() {
 	std::tr1::unordered_map<std::string, std::tr1::unordered_map<std::string, std::string> > serverConfig = config.getServerConfig();
 	for (std::tr1::unordered_map<std::string, std::tr1::unordered_map<std::string, std::string> >::iterator serverIterator = serverConfig.begin(); serverIterator != serverConfig.end(); serverIterator++) {
 		std::tr1::unordered_map<std::string, std::string> thisServerConf;
@@ -25,7 +27,7 @@ void connectServers(ConfigReader& config, ModuleInterface& modInterface) {
 	}
 }
 
-inline void loadModules(ConfigReader& config, ModuleInterface& modInterface) {
+inline void loadModules() {
 	std::tr1::unordered_map<std::string, std::tr1::unordered_map<std::string, std::string> > moduleConfig = config.getModConfig();
 	for (std::tr1::unordered_map<std::string, std::tr1::unordered_map<std::string, std::string> >::iterator modIter = moduleConfig.begin(); modIter != moduleConfig.end(); modIter++) {
 		std::string modName = modIter->first;
@@ -52,10 +54,8 @@ inline void loadModules(ConfigReader& config, ModuleInterface& modInterface) {
 }
 
 int main(int argc, char** argv) {
-	ConfigReader config;
-	ModuleInterface modInterface (&connectedServers, &loadedModules);
-	makeServerList(config);
-	connectServers(config, modInterface);
-	loadModules(config, modInterface);
+	makeServerList();
+	connectServers();
+	loadModules();
 	pthread_exit(NULL);
 }
