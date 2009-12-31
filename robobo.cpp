1570f2c276861d595ee99c536992f559e55486f6
ElementalAlchemist <ElementAlchemist7@gmail.com>
2009-12-30 20:51:41 -0500

    Start reading arguments.

--- robobo.cpp
+++ robobo.cpp
@@ -1,6 +1,16 @@
 #include "robobo.h"
 
 int main(int argc, char** argv) {
+	if (argc > 1) { // analyze arguments
+		if (strcmp(argv[1], "--help") == 0) {
+			// put help here eventually
+			return 0;
+		}
+		if (strcmp(argv[1], "--version") == 0) {
+			std::cout << "RoBoBo-IRC-BoBo Pre-alpha Development Version" << std::endl;
+			return 0;
+		}
+	}
 	ConfigReader config;
 	new ModuleInterface (&config);
 	pthread_exit(NULL);
