a67dd63b02cdce81d88c58132ae20d57bc300072
ElementalAlchemist <ElementAlchemist7@gmail.com>
2010-01-10 23:48:40 -0500

    Add m_ctcpversionreply

--- Makefile
+++ Makefile
@@ -7,10 +7,12 @@ debug:
 	g++ -ansi -pedantic-errors -Wall -ldl -pthread -rdynamic -g -O0 -fno-inline robobo.cpp -o robobo
 	g++ -ansi -pedantic-errors -Wall -pipe -shared -fPIC -g modules/m_helloworld.cpp -o modules/m_helloworld.so
 	g++ -ansi -pedantic-errors -Wall -pipe -shared -fPIC -g modules/m_modetest.cpp -o modules/m_modetest.so
+	g++ -ansi -pedantic-errors -Wall -pipe -shared -fPIC -g modules/m_ctcpversionreply.cpp -o modules/m_ctcpversionreply.so
 robobo:
 	g++ -ansi -pedantic-errors -Wall -ldl -pthread -rdynamic robobo.cpp -o robobo
 	g++ -ansi -pedantic-errors -Wall -pipe -shared -fPIC modules/m_helloworld.cpp -o modules/m_helloworld.so
 	g++ -ansi -pedantic-errors -Wall -pipe -shared -fPIC modules/m_modetest.cpp -o modules/m_modetest.so
+	g++ -ansi -pedantic-errors -Wall -pipe -shared -fPIC modules/m_ctcpversionreply.cpp -o modules/m_ctcpversionreply.so
 clean:
 	rm modules/*.so
 	rm robobo
---
+++ modules/m_ctcpversionreply.cpp
@@ -0,0 +1,50 @@
+#include "modinclude.h"
+
+class VersionReply : public Module {
+	public:
+		void onChannelCTCP(std::string server, std::string channel, char target, std::string nick, std::string message);
+		void onUserCTCP(std::string server, std::string nick, std::string message);
+		std::string getDesc();
+	private:
+		void sendVersionReply(std::string server, std::string target);
+};
+
+void VersionReply::onChannelCTCP(std::string server, std::string channel, char target, std::string nick, std::string message) {
+	if (splitBySpace(message)[0] == "VERSION")
+		sendVersionReply(server, nick);
+}
+
+void VersionReply::onUserCTCP(std::string server, std::string nick, std::string message) {
+	if (splitBySpace(message)[0] == "VERSION")
+		sendVersionReply(server, nick);
+}
+
+void VersionReply::sendVersionReply(std::string server, std::string target) {
+	std::string versionReply = "RoBoBo-IRC-BoBo IRC Bot ";
+	switch (getBotVersion()) { // get bot version, so this mod isn't restricted to one version of the bot and doesn't have to be updated with each version
+		case 900:
+			versionReply += "v1.0.0a1";
+			break;
+		case 950:
+			versionReply += "v1.0.0b1";
+			break;
+		case 1000:
+			versionReply += "v1.0.0";
+			break;
+	} // More of these may be added as they exist.
+	if (config["replystring"] != "")
+		versionReply += " " + config["replystring"];
+	sendCTCPReply(server, target, "VERSION", versionReply);
+}
+
+std::string VersionReply::getDesc() {
+	return "This module sends a reply to CTCP version.";
+}
+
+extern "C" Module* spawn() {
+	return new VersionReply;
+}
+
+extern "C" void unspawn(Module* mod) {
+	delete mod;
+}
