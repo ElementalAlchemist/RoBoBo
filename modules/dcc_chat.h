d569c69ea1a361ada0e09f2c58bd611b686d82e9
ElementalAlchemist <ElementAlchemist7@gmail.com>
2010-01-16 18:26:44 -0500

    Fix those functions not being virtual.

--- modules/dcc_chat.h
+++ modules/dcc_chat.h
@@ -1,9 +1,10 @@
 class dccChat : public Module {
 	public:
-		void onDCCReceive(std::string dccid, std::string message);
+		virtual void onDCCReceive(std::string dccid, std::string message);
 };
 
 class dccSender : public Module {
 	public:
-		void dccSend(std::string dccid, std::string message);
+		virtual void dccSend(std::string dccid, std::string message);
+		virtual bool hookDCCMessage(std::string modName, std::string hookMsg);
 };
