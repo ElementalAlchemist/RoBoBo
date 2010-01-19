6b5721b6139e81cae994feb41e3e84fd951cb4a9
ElementalAlchemist <ElementAlchemist7@gmail.com>
2010-01-19 12:47:22 -0500

    Virtual destructors.

--- modules/dcc_chat.h
+++ modules/dcc_chat.h
@@ -1,11 +1,13 @@
 class dccChat : public Module {
 	public:
+		virtual ~dccChat();
 		virtual void onDCCReceive(std::string dccid, std::string message);
 		virtual void onDCCEnd(std::string dccid);
 };
 
 class dccSender : public Module {
 	public:
+		virtual ~dccSender();
 		virtual void dccSend(std::string dccid, std::string message);
 		virtual bool hookDCCMessage(std::string modName, std::string hookMsg);
 };
