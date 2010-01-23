2464ee0c93fde5cf6d0981ba784ae9b57f042359
ElementalAlchemist <ElementAlchemist7@gmail.com>
2010-01-23 18:58:34 -0500

    Add definitions to the DCC chat module header.

--- modules/dcc_chat.h
+++ modules/dcc_chat.h
@@ -12,3 +12,17 @@ class dccSender : public Module {
 		virtual bool hookDCCMessage(std::string modName, std::string hookMsg);
 		virtual void unhookDCCSession(std::string modName, std::string dccid);
 };
+
+dccChat::~dccChat() {}
+
+void dccChat::onDCCReceive(std::string dccid, std::string message) {}
+
+void dccChat::onDCCEnd(std::string dccid) {}
+
+dccSender::~dccSender() {}
+
+void dccSender::dccSend(std::string dccid, std::string message) {}
+
+bool dccSender::hookDCCMessage(std::string modName, std::string hookMsg) { return false; }
+
+void dccSender::unhookDCCSession(std::string modName, std::string dccid) {}
