8f16586fe750fcfede9568aa01469940b7773c2f
ElementalAlchemist <ElementAlchemist7@gmail.com>
2010-05-27 14:42:16 -0400

    Fix broken modules makefile

--- configure.cpp
+++ configure.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char** argv) {
 	std::string mainMakefile = "CC = g++\nLIBS = -ldl -lpthread\nLDFLAGS=$(LIBS) -rdynamic\nCXXFLAGS=-Wall -pthread $(DEBUG)\nSOURCES=modinterface.cpp server.cpp channel.cpp config.cpp modules.cpp server.cpp socket.cpp user.cpp\n\ndefault: makedepend robobo modules\n\n";
 	mainMakefile += "makedepend:\n\tfor i in *.cpp; do for inc in  $(awk '/^#include \"/ {print $2}' $i | sed 's/\"//g'); do echo \"${i%.cpp}.o: $inc\"; done; done\n\ndebug:\n\tDEBUG=\"-g -O0\" make -C .\n\nrobobo: $(SOURCES:.cpp=.o)\n\n.PHONY: modules clean\n\nmodules:\n\tmake -C modules/\n\nclean:\n\tmake -C modules/ clean\n\trm -f robobo\n\trm -f $(SOURCES:.cpp=.o)";
-	std::string moduleMakefileBegin = "CXX = g++\nCXXFLAGS=-fPIC -Wall -pipe -ansi -pedantic-errors $(DEBUG)\nLDFLAGS=-shared\nDEPS=modinclude.h ../main.h ../connection.h ../modules.h ../config.cpp ../modules.cpp ../modinterface.cpp\n\nall: modules\n\nmodules:\n";
+	std::string moduleMakefileBegin = "CXX = g++\nCXXFLAGS=-fPIC -Wall -pipe -ansi -pedantic-errors $(DEBUG)\nLDFLAGS=-shared\nDEPS=modinclude.h ../main.h ../connection.h ../modules.h ../config.cpp ../modules.cpp ../modinterface.cpp\n\nall: ";
 	std::string moduleMakefileEnd = ".PHONY: clean\n\nclean:\n\trm -f *.o *.so";
 	std::ofstream makeOut;
 	std::string dir = argv[0];
@@ -31,6 +31,9 @@ int main(int argc, char** argv) {
 	}
 	makeOut.open(modmakedir.c_str(), std::ios::out | std::ios::trunc);
 	makeOut << moduleMakefileBegin;
+	for (unsigned int i = 0; i < moduleFiles.size(); i++)
+		makeOut << moduleFiles[i] << ".so ";
+	makeOut << "\n\n";
 	for (unsigned int i = 0; i < moduleFiles.size(); i++)
 		makeOut << moduleFiles[i] << ".so: " << moduleFiles[i] << ".o\n\t$(CXX) $(LDFLAGS) $^ -o $@\n";
 	for (unsigned int i = 0; i < moduleFiles.size(); i++)
