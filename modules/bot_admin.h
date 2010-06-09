d4b5728e47d76c8c082f4226b55ea2819f3051eb
ElementalAlchemist <ElementAlchemist7@gmail.com>
2010-06-09 14:27:05 -0400

    Add documentation for module interaction in the header files.

--- modules/bot_admin.h
+++ modules/bot_admin.h
@@ -16,9 +16,46 @@ class AdminMod : public dccChat {
 AdminHook::~AdminHook() {}
 
 std::vector<std::vector<std::string> > AdminHook::adminCommands() { return std::vector<std::vector<std::string> > (); }
+/* adminCommands(std::vector<std::vector<std::string> >)
+This function allows modules to specify which admin commands they provide.  This allows the admin module to call the
+admin command function (onAdminCommand) when one of the commands is given.  The function should return a vector of vectors
+of strings, as such:
+vector -> {
+	vector -> {
+		"command1" // the command itself, in all lowercase
+		"This is a sample command that actually does absolutely nothing." // a short description of the command
+		"Syntax: command1" // Elements 2 and above in the vector are the help for the command
+		"This command secretly does nothing." // Each element is sent as one line of the help
+		"But don't tell anyone! ;)"
+	}
+	vector -> {
+		"anothercommand" // this bot supports two commands
+		"This command calls the first command."
+		"Syntax: anothercommand"
+		"Blah."
+	}
+}
+Using push_back, the strings should be added to the inner vector in that order.  Vectors added to the outer vector can
+be added in any order.
+*/
 
 void AdminHook::onAdminCommand(std::string server, std::string nick, std::string command, std::string message, dccSender* dccMod, bool master) {}
+/* onAdminCommand(std::string, std::string, std::string, std::string, dccSender*, bool)
+This function is called by the admin module whenever a command that you told the admin module in adminCommands().
+The parameters are provided as follows:
+- server: the server (as known by the bot) to which the user sending the command is connected
+- nick: the nick the user sending the command is using
+- command: the command given (you won't need to check this if you only gave one command in adminCommands())
+- message: All of the parameters given to the command (i.e. the entire message starting immediately after the space after the command)
+- dccMod: A pointer to the DCC chat module.  If the command is given via DCC, the dccMod is a pointer to the DCC mod.  Using the default DCC
+	module provided by the bot distribution, the ID you would send to to reply to the command is server/nick.
+	If the command is not given to the admin via DCC, the dccMod pointer will be NULL.
+- master: Whether a bot master sent the command.  If true, the bot master sent the command.  If false, an admin who is not the bot master sent it.
+*/
 
 AdminMod::~AdminMod() {}
 
 void AdminMod::sendVerbose(int verboseLevel, std::string message) {}
+/* sendVerbose(int, std::string)
+This function sends the message specified to all admins with a verbose level of at least verboseLevel.
+*/
--- modules/dcc_chat.h
+++ modules/dcc_chat.h
@@ -18,19 +18,50 @@ class dccSender : public Module {
 dccChat::~dccChat() {}
 
 void dccChat::onDCCReceive(std::string dccid, std::string message) {}
+/* onDCCReceive(std::string, std::string)
+This function is called whenever the DCC chat module receives a DCC message that should be sent to your module
+(the message starting with hookMsg and all messages after until unhookDCCSession is called).  It gives the DCC
+ID of the sender (using the default DCC chat module, server/nick) and the message sent to the bot.
+*/
 
 void dccChat::onDCCEnd(std::string dccid) {}
+/* onDCCEnd(std::string)
+This function is called when a DCC chat session is terminated.  The DCC ID of the user whose chat was terminated
+is provided.
+*/
 
 dccSender::~dccSender() {}
 
 void dccSender::dccSend(std::string dccid, std::string message) {}
+/* dccSend(std::string, std::string)
+This function tells the DCC chat module to send the provided message to the DCC session associated with the provided
+DCC ID.
+*/
 
 bool dccSender::hookDCCMessage(std::string modName, std::string hookMsg) { return false; }
+/* hookDCCMessage(std::string, std::string)
+This function tells the DCC chat module to hook any message starting with hookMsg to the module modName.  To hook messages
+to the module calling the function, modName should be sent as the Module variable moduleName, which is provided in every
+module as the internal bot name for your module.
+This function returns true if the message could be hooked, and false if it couldn't.
+*/
 
 void dccSender::unhookDCCSession(std::string modName, std::string dccid) {}
+/* unhookDCCSession(std::string, std::string)
+This function tells the DCC chat module that it shouldn't send any more messages from the DCC chat session associated with
+the provided DCC ID to the module modName.  To unhook the DCC session from the module calling the function, the module should
+use the Module variable moduleName, which is provided in every module as the internal bot name for your module.
+*/
 
 std::vector<std::string> dccSender::getConnections() {
 	return std::vector<std::string> ();
 }
+/* getConnections()
+This function returns a vector of the DCC IDs of all connected DCC chat sessions.
+*/
 
 void dccSender::closeDCCConnection(std::string dccid) {}
+#include <unistd.h>
+/* closeDCCConnection(std::string)
+This function causes the bot to close the DCC connection associated with the given DCC ID.
+*/
