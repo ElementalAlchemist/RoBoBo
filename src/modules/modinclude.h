35fda4d79df682eb5225243bdabfafc10b709692
ElementalAlchemist <ElementAlchemist7@gmail.com>
2011-05-04 10:58:20 -0400

    Update modinclude header

--- src/modules/modinclude.h
+++ src/modules/modinclude.h
@@ -3,8 +3,6 @@
 #include "../main.h"
 
 class ConfigReader;
-class ModuleInterface; // forward-declare so it can be used in modules and server
-#include "../connection.h" // declare other classes that use it
 #include "../modules.h"
 #include "../config.h"
 #endif
