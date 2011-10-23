7abe130893a04b583a0b5b48f4710b881de9fc6b
ElementalAlchemist <ElementAlchemist7@gmail.com>
2011-10-23 11:50:34 -0400

    Fix CAP header file

--- src/modules/cap.h
+++ src/modules/cap.h
@@ -1,18 +1,22 @@
 class CapModule : public Module {
 	public:
 		CapModule(std::tr1::unordered_map<std::string, std::string> modConf, Base* modFace, std::string modName, std::string dir, unsigned short debug);
-		void registerCap(std::string moduleName, std::string capCommand);
-		void blockCap(std::string server, std::string moduleName);
-		void continueCap(std::string server, std::string moduleName);
+		virtual ~CapModule();
+		virtual void registerCap(std::string moduleName, std::string capCommand);
+		virtual void blockCap(std::string server, std::string moduleName);
+		virtual void continueCap(std::string server, std::string moduleName);
 };
 
 class CapClient : public Module {
 	public:
 		CapClient(std::tr1::unordered_map<std::string, std::string> modConf, Base* modFace, std::string modName, std::string dir, unsigned short debug);
-		void onCapAccept(std::string server, std::string capCommand);
+		virtual ~CapClient();
+		virtual void onCapAccept(std::string server, std::string capCommand);
 };
 
 CapModule::CapModule(std::tr1::unordered_map<std::string, std::string> modConf, Base* modFace, std::string modName, std::string dir, unsigned short debug) : Module(modConf, modFace, modName, dir, debug) {}
+
+CapModule::~CapModule() {}
 
 void CapModule::registerCap(std::string moduleName, std::string capCommand) {}
 
@@ -21,5 +25,7 @@ void CapModule::blockCap(std::string server, std::string moduleName) {}
 void CapModule::continueCap(std::string server, std::string moduleName) {}
 
 CapClient::CapClient(std::tr1::unordered_map<std::string, std::string> modConf, Base* modFace, std::string modName, std::string dir, unsigned short debug) : Module(modConf, modFace, modName, dir, debug) {}
+
+CapClient::~CapClient() {}
 
 void CapClient::onCapAccept(std::string server, std::string capCommand) {}
