001e9a4ce20c5312bed84eb4a672aa80bd8b83fa
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-05-18 21:19:46 -0400

    More fully define the Base class

--- src/base.cpp
+++ src/base.cpp
@@ -1,3 +1,53 @@
 #include "base.h"
 
-Base::Base(std::string working, std::string config, std::string configFileName, unsigned short debug, bool log) : workingDir(working), configDir(config), configName(configFileName), debugLevel(debug), logDump(log) {}
+Base::Base(std::string working, std::string config, std::string configFileName, unsigned short debug, bool log) : workingDir(working), configDir(config), configName(configFileName), debugLevel(debug), logDump(log), startup(true) {}
+
+Base::~Base() {
+	// TODO: destroy things
+}
+
+void Base::readConfiguration() {
+	// TODO: parse config file
+}
+
+void Base::startQueueThread() {
+	// TODO: start message queue thread once module API is started
+}
+
+void Base::loadModules() {
+	for (std::string modName : startupModules)
+		loadModule(modName);
+}
+
+void Base::connectServers() {
+	for (std::string serverName : startupServers)
+		connectServer(serverName);
+}
+
+void Base::checkModules() {
+	// TODO: module and server check
+}
+
+void Base::unloadEverything() {
+	for (std::pair<std::string, Protocol*> server : servers)
+		disconnectServer(server.first);
+	for (std::pair<std::string, Module*> module : modules)
+		unloadModule(module.first);
+}
+
+LoadResult Base::loadModule(std::string modName) {
+	// TODO: load modules
+	return LOAD_SUCCESS;
+}
+
+void Base::unloadModule(std::string modName) {
+	// TODO: also unload modules
+}
+
+void Base::connectServer(std::string server) {
+	// TODO: basically everything
+}
+
+void Base::disconnectServer(std::string server) {
+	// TODO: pretty much, what did you expect?
+}
--- src/base.h
+++ src/base.h
@@ -14,12 +14,17 @@ class Base {
 		void unloadEverything();
 		// TODO: Module hooks
 		// TODO: Protocol hooks
+		LoadResult loadModule(std::string modName);
+		void unloadModule(std::string modName);
+		void connectServer(std::string server);
+		void disconnectServer(std::string server);
 	private:
 		const std::string workingDir, configDir, configName;
 		const unsigned short debugLevel;
 		const bool logDump;
+		bool startup;
 		std::unordered_map<std::string, std::unordered_map<std::string, std::string>> serverConfig, moduleConfig;
-		std::list<std::string> startupServers;
+		std::list<std::string> startupServers, startupModules;
 		std::map<std::string, Protocol*> servers;
 		std::map<std::string, Module*> modules;
 		std::unordered_map<std::string, void*> moduleFiles;
