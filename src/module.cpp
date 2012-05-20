4fe940ded9ae9aedd139054fe46f367883d7b9a1
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-05-20 14:41:59 -0400

    Add either all or most of the module hooks

--- src/module.cpp
+++ src/module.cpp
@@ -13,6 +13,108 @@ void Module::onModuleLoad(std::string modName) {}
 
 void Module::onModuleUnload(std::string modName) {}
 
+MsgAction Module::onChanMsg(std::string server, std::string client, std::string channel, char status, std::string nick, std::string message) {
+	return MSG_CONTINUE;
+}
+
+MsgAction Module::onUserMsg(std::string server, std::string client, std::string nick, std::string message) {
+	return MSG_CONTINUE;
+}
+
+MsgAction Module::onChanNotice(std::string server, std::string client, std::string channel, char status, std::string nick, std::string message) {
+	return MSG_CONTINUE;
+}
+
+MsgAction Module::onUserNotice(std::string server, std::string client, std::string nick, std::string message) {
+	return MSG_CONTINUE;
+}
+
+MsgAction Module::onChanCTCP(std::string server, std::string client, std::string channel, char status, std::string nick, std::string ctcp, std::string data) {
+	return MSG_CONTINUE;
+}
+
+MsgAction Module::onUserCTCP(std::string server, std::string client, std::string nick, std::string ctcp, std::string data) {
+	return MSG_CONTINUE;
+}
+
+MsgAction Module::onChanCTCPReply(std::string server, std::string client, std::string channel, char status, std::string nick, std::string ctcp, std::string data) {
+	return MSG_CONTINUE;
+}
+
+MsgAction Module::onUserCTCPReply(std::string server, std::string client, std::string nick, std::string ctcp, std::string data) {
+	return MSG_CONTINUE;
+}
+
+void Module::onChanModeAdd(std::string server, std::string client, std::string channel, std::string mode, std::string param) {}
+
+void Module::onChanModeRemove(std::string server, std::string client, std::string channel, std::string mode, std::string param) {}
+
+void Module::onUserModeAdd(std::string server, std::string client, std::string mode) {}
+
+void Module::onUserModeRemove(std::string server, std::string client, std::string mode) {}
+
+void Module::onUserSNOMaskAdd(std::string server, std::string client, char snomask) {}
+
+void Module::onUserSNOMaskRemove(std::string server, std::string client, char snomask) {}
+
+void Module::onChanTopic(std::string server, std::string client, std::string channel, std::string topic) {}
+
+void Module::onChanJoin(std::string server, std::string client, std::string channel, std::string nick) {}
+
+void Module::onChanPart(std::string server, std::string client, std::string channel, std::string nick, std::string reason) {}
+
+void Module::onUserConnect(std::string server, std::string client, std::string nick) {}
+
+void Module::onUserQuit(std::string server, std::string client, std::string nick, std::string reason) {}
+
+void Module::onNumeric(std::string server, std::string client, std::string numeric, std::vector<std::string> data) {}
+
+void Module::onOper(std::string server, std::string nick, std::string operType) {}
+
+void Module::onSNotice(std::string server, std::string snotype, std::string message) {}
+
+void Module::onUserMetadata(std::string server, std::string nick, std::string dataKey, std::string dataValue) {}
+
+void Module::onServerData(std::string server, std::string dataType, std::string params) {}
+
+void Module::onXLineAdd(std::string server, std::string lineType, std::string mask, std::string setter, time_t expiry, std::string reason) {}
+
+void Module::onXLineRemove(std::string server, std::string lineType, std::string mask) {}
+
+void Module::onOtherData(std::string server, std::string client, std::vector<std::string> lineTokens) {}
+
+void Module::onChanMsgOut(std::string server, std::string client, std::string channel, char status, std::string &message) {}
+
+void Module::onChanMsgSend(std::string server, std::string client, std::string channel, char status, std::string message) {}
+
+void Module::onUserMsgOut(std::string server, std::string client, std::string nick, std::string &message) {}
+
+void Module::onUserMsgSend(std::string server, std::string client, std::string nick, std::string message) {}
+
+void Module::onChanNoticeOut(std::string server, std::string client, std::string channel, char status, std::string &message) {}
+
+void Module::onChanNoticeSend(std::string server, std::string client, std::string channel, char status, std::string message) {}
+
+void Module::onUserNoticeOut(std::string server, std::string client, std::string nick, std::string &message) {}
+
+void Module::onUserNoticeSend(std::string server, std::string client, std::string nick, std::string message) {}
+
+void Module::onChanCTCPOut(std::string server, std::string client, std::string channel, char status, std::string &ctcp, std::string &params) {}
+
+void Module::onChanCTCPSend(std::string server, std::string client, std::string channel, char status, std::string ctcp, std::string params) {}
+
+void Module::onUserCTCPOut(std::string server, std::string client, std::string nick, std::string &ctcp, std::string &params) {}
+
+void Module::onUserCTCPSend(std::string server, std::string client, std::string nick, std::string ctcp, std::string params) {}
+
+void Module::onChanCTCPReplyOut(std::string server, std::string client, std::string channel, char status, std::string &ctcp, std::string &params) {}
+
+void Module::onChanCTCPReplySend(std::string server, std::string client, std::string channel, char status, std::string ctcp, std::string params) {}
+
+void Module::onUserCTCPReplyOut(std::string server, std::string client, std::string nick, std::string &ctcp, std::string &params) {}
+
+void Module::onUserCTCPReplySend(std::string server, std::string client, std::string nick, std::string ctcp, std::string params) {}
+
 std::string Module::description() {
 	return "A description has not been provided.";
 }
--- src/module.h
+++ src/module.h
@@ -20,7 +20,50 @@ class Module {
 		virtual void onModuleLoad(std::string modName);
 		virtual void onModuleUnload(std::string modName);
 		
-		// TODO: module hooks
+		virtual MsgAction onChanMsg(std::string server, std::string client, std::string channel, char status, std::string nick, std::string message);
+		virtual MsgAction onUserMsg(std::string server, std::string client, std::string nick, std::string message);
+		virtual MsgAction onChanNotice(std::string server, std::string client, std::string channel, char status, std::string nick, std::string message);
+		virtual MsgAction onUserNotice(std::string server, std::string client, std::string nick, std::string message);
+		virtual MsgAction onChanCTCP(std::string server, std::string client, std::string channel, char status, std::string nick, std::string ctcp, std::string data);
+		virtual MsgAction onUserCTCP(std::string server, std::string client, std::string nick, std::string ctcp, std::string data);
+		virtual MsgAction onChanCTCPReply(std::string server, std::string client, std::string channel, char status, std::string nick, std::string ctcp, std::string data);
+		virtual MsgAction onUserCTCPReply(std::string server, std::string client, std::string nick, std::string ctcp, std::string data);
+		virtual void onChanModeAdd(std::string server, std::string client, std::string channel, std::string mode, std::string param);
+		virtual void onChanModeRemove(std::string server, std::string client, std::string channel, std::string mode, std::string param);
+		virtual void onUserModeAdd(std::string server, std::string client, std::string mode);
+		virtual void onUserModeRemove(std::string server, std::string client, std::string mode);
+		virtual void onUserSNOMaskAdd(std::string server, std::string client, char snomask);
+		virtual void onUserSNOMaskRemove(std::string server, std::string client, char snomask);
+		virtual void onChanTopic(std::string server, std::string client, std::string channel, std::string topic);
+		virtual void onChanJoin(std::string server, std::string client, std::string channel, std::string nick);
+		virtual void onChanPart(std::string server, std::string client, std::string channel, std::string nick, std::string reason);
+		virtual void onUserConnect(std::string server, std::string client, std::string nick);
+		virtual void onUserQuit(std::string server, std::string client, std::string nick, std::string reason);
+		virtual void onNumeric(std::string server, std::string client, std::string numeric, std::vector<std::string> data);
+		virtual void onOper(std::string server, std::string nick, std::string operType);
+		virtual void onSNotice(std::string server, std::string snotype, std::string message);
+		virtual void onUserMetadata(std::string server, std::string nick, std::string dataKey, std::string dataValue);
+		virtual void onServerData(std::string server, std::string dataType, std::string params);
+		virtual void onXLineAdd(std::string server, std::string lineType, std::string mask, std::string setter, time_t expiry, std::string reason);
+		virtual void onXLineRemove(std::string server, std::string lineType, std::string mask);
+		virtual void onOtherData(std::string server, std::string client, std::vector<std::string> lineTokens);
+		
+		virtual void onChanMsgOut(std::string server, std::string client, std::string channel, char status, std::string &message);
+		virtual void onChanMsgSend(std::string server, std::string client, std::string channel, char status, std::string message);
+		virtual void onUserMsgOut(std::string server, std::string client, std::string nick, std::string &message);
+		virtual void onUserMsgSend(std::string server, std::string client, std::string nick, std::string message);
+		virtual void onChanNoticeOut(std::string server, std::string client, std::string channel, char status, std::string &message);
+		virtual void onChanNoticeSend(std::string server, std::string client, std::string channel, char status, std::string message);
+		virtual void onUserNoticeOut(std::string server, std::string client, std::string nick, std::string &message);
+		virtual void onUserNoticeSend(std::string server, std::string client, std::string nick, std::string message);
+		virtual void onChanCTCPOut(std::string server, std::string client, std::string channel, char status, std::string &ctcp, std::string &params);
+		virtual void onChanCTCPSend(std::string server, std::string client, std::string channel, char status, std::string ctcp, std::string params);
+		virtual void onUserCTCPOut(std::string server, std::string client, std::string nick, std::string &ctcp, std::string &params);
+		virtual void onUserCTCPSend(std::string server, std::string client, std::string nick, std::string ctcp, std::string params);
+		virtual void onChanCTCPReplyOut(std::string server, std::string client, std::string channel, char status, std::string &ctcp, std::string &params);
+		virtual void onChanCTCPReplySend(std::string server, std::string client, std::string channel, char status, std::string ctcp, std::string params);
+		virtual void onUserCTCPReplyOut(std::string server, std::string client, std::string nick, std::string &ctcp, std::string &params);
+		virtual void onUserCTCPReplySend(std::string server, std::string client, std::string nick, std::string ctcp, std::string params);
 		
 		virtual std::string description();
 		virtual std::list<std::string> provides();
