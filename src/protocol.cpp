2a5bae893ad37a36c3086c0d477f718b7cdb3ce5
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-05-30 00:53:41 -0400

    Add a protocol API

---
+++ src/protocol.cpp
@@ -0,0 +1,281 @@
+#include "protocol.h"
+
+Protocol::Protocol(std::string server, std::map<std::string, std::string> conf, std::string workDir, short unsigned int debug, Base* botptr) : serverName(server), workingDir(workDir), config(conf), debugLevel(debug), bot(botptr) {}
+
+Protocol::~Protocol() {}
+
+void Protocol::connectServer() {}
+
+void Protocol::sendPrivMsg(std::string client, std::string target, std::string message) {}
+
+void Protocol::sendNotice(std::string client, std::string target, std::string message) {}
+
+void Protocol::sendCTCP(std::string client, std::string target, std::string ctcp) {}
+
+void Protocol::sendCTCPReply(std::string client, std::string target, std::string ctcp) {}
+
+void Protocol::setMode(std::string client, std::string target, std::list<std::string> setModes, std::list<std::string> delModes) {}
+
+void Protocol::setSNOMask(std::string client, std::string snomask) {}
+
+void Protocol::setChanTopic(std::string client, std::string channel, std::string topic) {}
+
+void Protocol::joinChannel(std::string client, std::string channel, std::string key) {}
+
+void Protocol::partChannel(std::string client, std::string channel, std::string reason) {}
+
+std::string Protocol::addClient(std::string nick, std::string ident, std::string host, std::string gecos) {
+	return "";
+}
+
+void Protocol::removeClient(std::string client) {}
+
+void Protocol::changeNick(std::string client, std::string newNick) {}
+
+void Protocol::oper(std::string client, std::string username, std::string password) {}
+
+void Protocol::sendSNotice(std::string snomask, std::string message) {}
+
+void Protocol::setMetadata(std::string target, std::string key, std::string value) {}
+
+void Protocol::setXLine(std::string client, std::string linetype, std::string mask, time_t duration, std::string reason) {}
+
+void Protocol::delXLine(std::string client, std::string linetype, std::string mask) {}
+
+void Protocol::sendOtherData(std::string client, std::string line) {}
+
+void Protocol::processedChanMsg(std::string client, std::string target, char status, std::string message) {}
+
+void Protocol::processedUserMsg(std::string client, std::string target, std::string message) {}
+
+void Protocol::processedChanNotice(std::string client, std::string target, char status, std::string message) {}
+
+void Protocol::processedUserNotice(std::string client, std::string target, std::string message) {}
+
+void Protocol::processedChanCTCP(std::string client, std::string target, char status, std::string ctcp) {}
+
+void Protocol::processedUserCTCP(std::string client, std::string target, std::string ctcp) {}
+
+void Protocol::processedChanCTCPReply(std::string client, std::string target, char status, std::string ctcp) {}
+
+void Protocol::processedUserCTCPReply(std::string client, std::string target, std::string ctcp) {}
+
+void Protocol::callChanMsgHook(std::string client, std::string channel, char status, std::string nick, std::string message) {
+	bot->modChanMsgHook(serverName, client, channel, status, nick, message);
+}
+
+void Protocol::callUserMsgHook(std::string client, std::string nick, std::string message) {
+	bot->modUserMsgHook(serverName, client, nick, message);
+}
+
+void Protocol::callChanNoticeHook(std::string client, std::string channel, char status, std::string nick, std::string message) {
+	bot->modChanNoticeHook(serverName, client, channel, status, nick, message);
+}
+
+void Protocol::callUserNoticeHook(std::string client, std::string nick, std::string message) {
+	bot->modUserNoticeHook(serverName, client, nick, message);
+}
+
+void Protocol::callChanCTCPHook(std::string client, std::string channel, char status, std::string nick, std::string ctcp) {
+	size_t spacePos = ctcp.find_first_of(' ');
+	if (spacePos == std::string::npos)
+		bot->modChanCTCPHook(serverName, client, channel, status, nick, ctcp, "");
+	else
+		bot->modChanCTCPHook(serverName, client, channel, status, nick, ctcp.substr(0, spacePos), ctcp.substr(spacePos + 1));
+}
+
+void Protocol::callUserCTCPHook(std::string client, std::string nick, std::string ctcp) {
+	size_t spacePos = ctcp.find_first_of(' ');
+	if (spacePos == std::string::npos)
+		bot->modUserCTCPHook(serverName, client, nick, ctcp, "");
+	else
+		bot->modUserCTCPHook(serverName, client, nick, ctcp.substr(0, spacePos), ctcp.substr(spacePos + 1));
+}
+
+void Protocol::callChanCTCPReplyHook(std::string client, std::string channel, char status, std::string nick, std::string ctcp) {
+	size_t spacePos = ctcp.find_first_of(' ');
+	if (spacePos == std::string::npos)
+		bot->modChanCTCPReplyHook(serverName, client, channel, status, nick, ctcp, "");
+	else
+		bot->modChanCTCPReplyHook(serverName, client, channel, status, nick, ctcp.substr(0, spacePos), ctcp.substr(spacePos + 1));
+}
+
+void Protocol::callUserCTCPReplyHook(std::string client, std::string nick, std::string ctcp) {
+	size_t spacePos = ctcp.find_first_of(' ');
+	if (spacePos == std::string::npos)
+		bot->modUserCTCPReplyHook(serverName, client, nick, ctcp, "");
+	else
+		bot->modUserCTCPReplyHook(serverName, client, nick, ctcp.substr(0, spacePos), ctcp.substr(spacePos + 1));
+}
+
+void Protocol::callChanModeHook(std::string client, std::string channel, bool add, std::string mode, std::string param) {
+	bot->modChanModeHook(serverName, client, channel, add, mode, param);
+}
+
+void Protocol::callUserModeHook(std::string client, bool add, std::string mode) {
+	bot->modUserModeHook(serverName, client, add, mode);
+}
+
+void Protocol::callUserSNOMaskHook(std::string client, bool add, std::string snomask) {
+	bot->modUserSNOMaskHook(serverName, client, add, snomask);
+}
+
+void Protocol::callChanTopicHook(std::string client, std::string channel, std::string topic) {
+	bot->modChanTopicHook(serverName, client, channel, topic);
+}
+
+void Protocol::callChanJoinHook(std::string client, std::string channel, std::string nick) {
+	bot->modChanJoinHook(serverName, client, channel, nick);
+}
+
+void Protocol::callChanPartHook(std::string client, std::string channel, std::string nick, std::string reason) {
+	bot->modChanPartHook(serverName, client, channel, nick, reason);
+}
+
+void Protocol::callUserConnectHook(std::string nick) {
+	bot->modUserConnectHook(serverName, nick);
+}
+
+void Protocol::callUserQuitHook(std::string client, std::string nick, std::string reason) {
+	bot->modUserQuitHook(serverName, client, nick, reason);
+}
+
+void Protocol::callUserNickHook(std::string client, std::string oldNick, std::string newNick) {
+	bot->modUserNickHook(serverName, client, oldNick, newNick);
+}
+
+void Protocol::callNumericHook(std::string client, std::string numeric, std::vector<std::string> data) {
+	bot->modNumericHook(serverName, client, numeric, data);
+}
+
+void Protocol::callOperHook(std::string nick, std::string operType) {
+	bot->modOperHook(serverName, nick, operType);
+}
+
+void Protocol::callSNoticeHook(std::string snotype, std::string message) {
+	bot->modSNoticeHook(serverName, snotype, message);
+}
+
+void Protocol::callMetadataHook(std::string target, std::string dataKey, std::string dataValue) {
+	bot->modMetadataHook(serverName, target, dataKey, dataValue);
+}
+
+void Protocol::callServerDataHook(std::string dataType, std::vector<std::string> params) {
+	bot->modServerDataHook(serverName, dataType, params);
+}
+
+void Protocol::callXLineAddHook(std::string lineType, std::string mask, std::string setter, time_t expiry, std::string reason) {
+	bot->modXLineAddHook(serverName, lineType, mask, setter, expiry, reason);
+}
+
+void Protocol::callXLineRemoveHook(std::string lineType, std::string mask) {
+	bot->modXLineRemoveHook(serverName, lineType, mask);
+}
+
+void Protocol::callServerConnectHook(std::string newServerName) {
+	bot->modServerConnectHook(serverName, newServerName);
+}
+
+void Protocol::callServerQuitHook(std::string quitServerName, std::string reason) {
+	bot->modServerQuitHook(serverName, quitServerName, reason);
+}
+
+void Protocol::callOtherDataHook(std::string client, std::vector<std::string> lineTokens) {
+	bot->modOtherDataHook(serverName, client, lineTokens);
+}
+
+void Protocol::callChanMsgOutHook(std::string client, std::string channel, char status, std::string message) {
+	bot->modChanMsgOutHook(serverName, client, channel, status, message);
+}
+
+void Protocol::callChanMsgSendHook(std::string client, std::string channel, char status, std::string message) {
+	bot->modChanMsgSendHook(serverName, client, channel, status, message);
+}
+
+void Protocol::callUserMsgOutHook(std::string client, std::string nick, std::string message) {
+	bot->modUserMsgOutHook(serverName, client, nick, message);
+}
+
+void Protocol::callUserMsgSendHook(std::string client, std::string nick, std::string message) {
+	bot->modUserMsgSendHook(serverName, client, nick, message);
+}
+
+void Protocol::callChanNoticeOutHook(std::string client, std::string channel, char status, std::string message) {
+	bot->modChanNoticeOutHook(serverName, client, channel, status, message);
+}
+
+void Protocol::callChanNoticeSendHook(std::string client, std::string channel, char status, std::string message) {
+	bot->modChanNoticeSendHook(serverName, client, channel, status, message);
+}
+
+void Protocol::callUserNoticeOutHook(std::string client, std::string nick, std::string message) {
+	bot->modUserNoticeOutHook(serverName, client, nick, message);
+}
+
+void Protocol::callUserNoticeSendHook(std::string client, std::string nick, std::string message) {
+	bot->modUserNoticeSendHook(serverName, client, nick, message);
+}
+
+void Protocol::callChanCTCPOutHook(std::string client, std::string channel, char status, std::string ctcp) {
+	size_t spacePos = ctcp.find_first_of(' ');
+	if (spacePos == std::string::npos)
+		bot->modChanCTCPOutHook(serverName, client, channel, status, ctcp, "");
+	else
+		bot->modChanCTCPOutHook(serverName, client, channel, status, ctcp.substr(0, spacePos), ctcp.substr(spacePos + 1));
+}
+
+void Protocol::callChanCTCPSendHook(std::string client, std::string channel, char status, std::string ctcp) {
+	size_t spacePos = ctcp.find_first_of(' ');
+	if (spacePos == std::string::npos)
+		bot->modChanCTCPSendHook(serverName, client, channel, status, ctcp, "");
+	else
+		bot->modChanCTCPSendHook(serverName, client, channel, status, ctcp.substr(0, spacePos), ctcp.substr(spacePos + 1));
+}
+
+void Protocol::callUserCTCPOutHook(std::string client, std::string nick, std::string ctcp) {
+	size_t spacePos = ctcp.find_first_of(' ');
+	if (spacePos == std::string::npos)
+		bot->modUserCTCPOutHook(serverName, client, nick, ctcp, "");
+	else
+		bot->modUserCTCPOutHook(serverName, client, nick, ctcp.substr(0, spacePos), ctcp.substr(spacePos + 1));
+}
+
+void Protocol::callUserCTCPSendHook(std::string client, std::string nick, std::string ctcp) {
+	size_t spacePos = ctcp.find_first_of(' ');
+	if (spacePos == std::string::npos)
+		bot->modUserCTCPSendHook(serverName, client, nick, ctcp, "");
+	else
+		bot->modUserCTCPSendHook(serverName, client, nick, ctcp.substr(0, spacePos), ctcp.substr(spacePos + 1));
+}
+
+void Protocol::callChanCTCPReplyOutHook(std::string client, std::string channel, char status, std::string ctcp) {
+	size_t spacePos = ctcp.find_first_of(' ');
+	if (spacePos == std::string::npos)
+		bot->modChanCTCPReplyOutHook(serverName, client, channel, status, ctcp, "");
+	else
+		bot->modChanCTCPReplyOutHook(serverName, client, channel, status, ctcp.substr(0, spacePos), ctcp.substr(spacePos + 1));
+}
+
+void Protocol::callChanCTCPReplySendHook(std::string client, std::string channel, char status, std::string ctcp) {
+	size_t spacePos = ctcp.find_first_of(' ');
+	if (spacePos == std::string::npos)
+		bot->modChanCTCPReplySendHook(serverName, client, channel, status, ctcp, "");
+	else
+		bot->modChanCTCPReplySendHook(serverName, client, channel, status, ctcp.substr(0, spacePos), ctcp.substr(spacePos + 1));
+}
+
+void Protocol::callUserCTCPReplyOutHook(std::string client, std::string nick, std::string ctcp) {
+	size_t spacePos = ctcp.find_first_of(' ');
+	if (spacePos == std::string::npos)
+		bot->modUserCTCPReplyOutHook(serverName, client, nick, ctcp, "");
+	else
+		bot->modUserCTCPReplyOutHook(serverName, client, nick, ctcp.substr(0, spacePos), ctcp.substr(spacePos + 1));
+}
+
+void Protocol::callUserCTCPReplySendHook(std::string client, std::string nick, std::string ctcp) {
+	size_t spacePos = ctcp.find_first_of(' ');
+	if (spacePos == std::string::npos)
+		bot->modUserCTCPReplySendHook(serverName, client, nick, ctcp, "");
+	else
+		bot->modUserCTCPReplySendHook(serverName, client, nick, ctcp.substr(0, spacePos), ctcp.substr(spacePos + 1));
+}
---
+++ src/protocol.h
@@ -0,0 +1,90 @@
+#pragma once
+#include "main.h"
+
+#include "base.h"
+
+class Protocol {
+	public:
+		Protocol(std::string server, std::map<std::string, std::string> conf, std::string workDir, unsigned short debug, Base* botptr);
+		virtual ~Protocol();
+		virtual unsigned int apiVersion() = 0;
+		virtual void connectServer();
+		
+		virtual void sendPrivMsg(std::string client, std::string target, std::string message);
+		virtual void sendNotice(std::string client, std::string target, std::string message);
+		virtual void sendCTCP(std::string client, std::string target, std::string ctcp);
+		virtual void sendCTCPReply(std::string client, std::string target, std::string ctcp);
+		virtual void setMode(std::string client, std::string target, std::list<std::string> setModes, std::list<std::string> delModes);
+		virtual void setSNOMask(std::string client, std::string snomask);
+		virtual void setChanTopic(std::string client, std::string channel, std::string topic);
+		virtual void joinChannel(std::string client, std::string channel, std::string key = "");
+		virtual void partChannel(std::string client, std::string channel, std::string reason);
+		virtual std::string addClient(std::string nick, std::string ident, std::string host, std::string gecos);
+		virtual void removeClient(std::string client);
+		virtual void changeNick(std::string client, std::string newNick);
+		virtual void oper(std::string client, std::string username, std::string password);
+		virtual void sendSNotice(std::string snomask, std::string message);
+		virtual void setMetadata(std::string target, std::string key, std::string value);
+		virtual void setXLine(std::string client, std::string linetype, std::string mask, time_t duration, std::string reason);
+		virtual void delXLine(std::string client, std::string linetype, std::string mask);
+		virtual void sendOtherData(std::string client, std::string line);
+		
+		virtual void processedChanMsg(std::string client, std::string target, char status, std::string message);
+		virtual void processedUserMsg(std::string client, std::string target, std::string message);
+		virtual void processedChanNotice(std::string client, std::string target, char status, std::string message);
+		virtual void processedUserNotice(std::string client, std::string target, std::string message);
+		virtual void processedChanCTCP(std::string client, std::string target, char status, std::string ctcp);
+		virtual void processedUserCTCP(std::string client, std::string target, std::string ctcp);
+		virtual void processedChanCTCPReply(std::string client, std::string target, char status, std::string ctcp);
+		virtual void processedUserCTCPReply(std::string client, std::string target, std::string ctcp);
+	protected:
+		std::string serverName, workingDir;
+		std::map<std::string, std::string> config;
+		unsigned short debugLevel;
+		void callChanMsgHook(std::string client, std::string channel, char status, std::string nick, std::string message);
+		void callUserMsgHook(std::string client, std::string nick, std::string message);
+		void callChanNoticeHook(std::string client, std::string channel, char status, std::string nick, std::string message);
+		void callUserNoticeHook(std::string client, std::string nick, std::string message);
+		void callChanCTCPHook(std::string client, std::string channel, char status, std::string nick, std::string ctcp);
+		void callUserCTCPHook(std::string client, std::string nick, std::string ctcp);
+		void callChanCTCPReplyHook(std::string client, std::string channel, char status, std::string nick, std::string ctcp);
+		void callUserCTCPReplyHook(std::string client, std::string nick, std::string ctcp);
+		void callChanModeHook(std::string client, std::string channel, bool add, std::string mode, std::string param);
+		void callUserModeHook(std::string client, bool add, std::string mode);
+		void callUserSNOMaskHook(std::string client, bool add, std::string snomask);
+		void callChanTopicHook(std::string client, std::string channel, std::string topic);
+		void callChanJoinHook(std::string client, std::string channel, std::string nick);
+		void callChanPartHook(std::string client, std::string channel, std::string nick, std::string reason);
+		void callUserConnectHook(std::string nick);
+		void callUserQuitHook(std::string client, std::string nick, std::string reason);
+		void callUserNickHook(std::string client, std::string oldNick, std::string newNick);
+		void callNumericHook(std::string client, std::string numeric, std::vector<std::string> data);
+		void callOperHook(std::string nick, std::string operType);
+		void callSNoticeHook(std::string snotype, std::string message);
+		void callMetadataHook(std::string target, std::string dataKey, std::string dataValue);
+		void callServerDataHook(std::string dataType, std::vector<std::string> params);
+		void callXLineAddHook(std::string lineType, std::string mask, std::string setter, time_t expiry, std::string reason);
+		void callXLineRemoveHook(std::string lineType, std::string mask);
+		void callServerConnectHook(std::string newServerName);
+		void callServerQuitHook(std::string quitServerName, std::string reason);
+		void callOtherDataHook(std::string client, std::vector<std::string> lineTokens);
+		void callChanMsgOutHook(std::string client, std::string channel, char status, std::string message);
+		void callChanMsgSendHook(std::string client, std::string channel, char status, std::string message);
+		void callUserMsgOutHook(std::string client, std::string nick, std::string message);
+		void callUserMsgSendHook(std::string client, std::string nick, std::string message);
+		void callChanNoticeOutHook(std::string client, std::string channel, char status, std::string message);
+		void callChanNoticeSendHook(std::string client, std::string channel, char status, std::string message);
+		void callUserNoticeOutHook(std::string client, std::string nick, std::string message);
+		void callUserNoticeSendHook(std::string client, std::string nick, std::string message);
+		void callChanCTCPOutHook(std::string client, std::string channel, char status, std::string ctcp);
+		void callChanCTCPSendHook(std::string client, std::string channel, char status, std::string ctcp);
+		void callUserCTCPOutHook(std::string client, std::string nick, std::string ctcp);
+		void callUserCTCPSendHook(std::string client, std::string nick, std::string ctcp);
+		void callChanCTCPReplyOutHook(std::string client, std::string channel, char status, std::string ctcp);
+		void callChanCTCPReplySendHook(std::string client, std::string channel, char status, std::string ctcp);
+		void callUserCTCPReplyOutHook(std::string client, std::string nick, std::string ctcp);
+		void callUserCTCPReplySendHook(std::string client, std::string nick, std::string ctcp);
+	private:
+		Base* bot;
+		Socket* connection;
+};
