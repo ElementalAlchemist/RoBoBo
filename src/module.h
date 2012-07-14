012deb9971021cb3b5c771e6117e775d5a6d7d7d
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-07-14 19:00:51 -0400

    Use implicit inlining of small Module functions.

--- src/module.h
+++ src/module.h
@@ -19,23 +19,23 @@ class Module {
 	public:
 		Module(std::string modName, std::map<std::string, std::string> conf, std::string workDir, unsigned short debug, Base* botptr);
 		virtual ~Module();
-		virtual Priority priority();
+		virtual Priority priority() { return PRI_NORMAL; }
 		virtual unsigned int apiVersion() = 0;
-		virtual bool onLoadComplete();
-		virtual void onUnload();
-		virtual void onRehash();
-		virtual void onModuleLoad(std::string modName);
-		virtual void onModuleUnload(std::string modName);
-		virtual bool forceKeepAlive();
-		void rehash(std::map<std::string, std::string> conf);
-		void endDebug();
+		virtual bool onLoadComplete() { return true; }
+		virtual void onUnload() {}
+		virtual void onRehash() {}
+		virtual void onModuleLoad(std::string modName) {}
+		virtual void onModuleUnload(std::string modName) {}
+		virtual bool forceKeepAlive() { return false; }
+		void rehash(std::map<std::string, std::string> conf) { config = conf; }
+		void endDebug() { debugLevel = 0; }
 		
 		
 		
-		virtual std::string description();
-		virtual std::list<std::string> provides();
-		virtual std::list<std::string> requires();
-		virtual std::list<std::string> supports();
+		virtual std::string description() { return "A description has not been provided by the module author."; }
+		virtual std::list<std::string> provides() { return std::list<std::string> (); }
+		virtual std::list<std::string> requires() { return std::list<std::string> (); }
+		virtual std::list<std::string> supports() { return std::list<std::string> (); }
 	protected:
 		const std::string moduleName, workingDir;
 		std::map<std::string, std::string> config;
