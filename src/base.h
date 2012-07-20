6ff5b18f3e11d8f079a6b067fd5dc606652b0532
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-07-20 18:41:47 -0400

    Fix the type in the class, too.

--- src/base.h
+++ src/base.h
@@ -104,7 +104,7 @@ class Base {
 		std::set<std::string> userModes(const std::string& server, const std::string& user);
 		bool userHasMode(const std::string& server, const std::string& user, const std::string& mode);
 		std::list<char> userSNOmasks(const std::string& server, const std::string& user);
-		bool userHasSNOmask(const std::string& server, const std::string& user, const std::string& mode);
+		bool userHasSNOmask(const std::string& server, const std::string& user, char snomask);
 		std::set<std::string> userChans(const std::string& server, const std::string& user);
 		time_t userTimestamp(const std::string& server, const std::string& user);
 		time_t userNickTimestamp(const std::string& server, const std::string& user);
