79d7064ffe2934ca88d82e7d1c4431fe74df16de
ElementalAlchemist <ElementAlchemist7@gmail.com>
2012-07-24 13:10:17 -0400

    Condense the module make rules generator a bit.

--- src/modules/module_make.sh
+++ src/modules/module_make.sh
@@ -1,5 +1,4 @@
 cd src/modules
-for source in *.cpp; do 
-	COMPILE_FLAGS=`grep '^s*// COMPILE_FLAGS ' $source | awk -F'[ ]' '{$1=$2=""; print $0}'`
-	printf "`g++ -std=c++0x -E -MM $source -MT ../../modules/${source:0:${#source}-4}.so`\n\t@echo \"\e[1;32mBuild:\e[0m\t$source\"\n\t@g++ -std=c++0x -Wall -pedantic -pthread -rdynamic $DEBUG -pipe -fPIC -shared $source -ldl $COMPILE_FLAGS -c -o ../../modules/${source:0:${#source}-4}.so\n\n"
+for source in *.cpp; do
+	printf "`g++ -std=c++0x -E -MM $source -MT ../../modules/${source:0:${#source}-4}.so`\n\t@echo \"\e[1;32mBuild:\e[0m\t$source\"\n\t@g++ -std=c++0x -Wall -pedantic -pthread -rdynamic $DEBUG -pipe -fPIC -shared $source -ldl `grep '^\s*// COMPILE_FLAGS ' $source | awk -F'[ ]' '{$1=$2=""; print $0}'` -c -o ../../modules/${source:0:${#source}-4}.so\n\n"
 done
