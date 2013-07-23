f6d2e9794816c4538b359112b7080d4b241bcc5b
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-07-22 21:46:37 -0400

    While I'm making incredible use of std::pair, I should probably add the header that contains it

--- src/main.h
+++ src/main.h
@@ -5,10 +5,14 @@
 #include <exception>
 #include <iostream>
 #include <list>
+#include <map>
+#include <memory>
+#include <set>
 #include <sstream>
 #include <string>
 #include <tuple>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
 const std::string BotVersion ("3.0.0-devel");
