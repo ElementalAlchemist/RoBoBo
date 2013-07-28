a47060a10ee0af92011e1bd255eaecfe85ebb00a
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-07-28 17:48:54 -0400

    Implement the SocketManager and a base Socket class

---
+++ src/modtypes/socket.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "../main.h"
+
+class Socket {
+	public:
+		virtual ~Socket() {}
+		virtual unsigned int apiVersion() = 0;
+		virtual void connect(const std::string& server, const std::string& port, const std::string& bind = "") {}
+		virtual std::string receive() { return ""; }
+		virtual void send(const std::string& data) {}
+		virtual void closeConnection() {}
+		bool isConnected() { return connected; }
+	protected:
+		bool connected;
+};
---
+++ src/socketmanager.cpp
@@ -0,0 +1,29 @@
+#include "socketmanager.h"
+
+std::shared_ptr<Socket> SocketManager::getSocket(const std::string& socketType) {
+	void* sockFile = dlopen(("sockets/" + socketType + ".so").c_str(), RTLD_NOW);
+	if (sockFile == nullptr)
+		throw SocketLoadFailed (dlerror());
+	void* spawnFunc = dlsym(sockFile, "spawn");
+	if (spawnFunc == nullptr) {
+		const char* loadError = dlerror();
+		dlclose(sockFile);
+		if (loadError)
+			throw SocketLoadFailed (loadError);
+		throw SocketLoadFailed ("The spawn symbol has been set to null, but it must be a valid function.");
+	}
+	Socket*(*spawnCallFunc)() = static_cast<Socket*(*)()> (spawnFunc);
+	std::shared_ptr<Socket> newSocket (spawnCallFunc(), std::bind(&SocketManager::removeSocket, this, socketType, sockFile, std::placeholders::_1));
+	if (apiVersions.find(newSocket->apiVersion()) == apiVersions.end()) {
+		dlclose(sockFile);
+		throw SocketAPIMismatch;
+	}
+	return newSocket;
+}
+
+void SocketManager::removeSocket(const std::string& socketType, void* sockFile, Socket* deletingSocket) {
+	delete deletingSocket;
+	dlclose(sockFile);
+	LogManager* logger = LogManager::getHandle();
+	logger->log(LOG_DEBUG, "socket-request", "A " + socketType + " socket has been unloaded.");
+}
