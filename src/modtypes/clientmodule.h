43f83c2de6edf04fc0f5535206cce3afdcf7b94f
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-08-01 13:40:09 -0400

    Since the modtypes files aren't compiled by themselves, only included in others, remove most of the includes so they don't interfere with other special include orders

--- src/modtypes/clientmodule.h
+++ src/modtypes/clientmodule.h
@@ -1,7 +1,5 @@
 #pragma once
 #include "../main.h"
-#include "../servermanager.h"
-#include "basemodule.h"
 
 class ClientModule {
 	public:
--- src/modtypes/clientprotocol.h
+++ src/modtypes/clientprotocol.h
@@ -1,7 +1,5 @@
 #pragma once
 #include "../main.h"
-#include "../modulemanager.h"
-#include "../socketmanager.h"
 
 class ClientProtocol {
 	public:
