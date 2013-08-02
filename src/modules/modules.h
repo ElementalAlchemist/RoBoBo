36d948c34310944a962903cb2d1eb318a8453b52
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-08-01 21:00:55 -0400

    Add spawn macros to module include files

--- src/modules/modules.h
+++ src/modules/modules.h
@@ -1,3 +1,8 @@
 #pragma once
 #include "../main.h"
 #include "../modulemanager.h"
+
+#define MODULE_SPAWN(ModuleClass) \
+extern "C" Module* spawn(const std::string& name) { \
+	return new ModuleClass (name); \
+}
--- src/sockets/sockets.h
+++ src/sockets/sockets.h
@@ -2,3 +2,8 @@
 #include "../main.h"
 #include "../modtypes/socket.h"
 #include "../socketmanager.h"
+
+#define SOCKET_SPAWN(SocketClass) \
+extern "C" Socket* spawn() { \
+	return new SocketClass; \
+}
