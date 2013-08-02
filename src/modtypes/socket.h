10e6aefe5410267b5998df8417a16a1674e2f12b
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-08-01 20:35:32 -0400

    Rename parameter to not be the same as the name of the function that does it

--- src/modtypes/socket.h
+++ src/modtypes/socket.h
@@ -5,7 +5,7 @@ class Socket {
 	public:
 		virtual ~Socket() {}
 		virtual unsigned int apiVersion() = 0;
-		virtual void connect(const std::string& server, const std::string& port, const std::string& bind = "") {}
+		virtual void connect(const std::string& server, const std::string& port, const std::string& bindAddr = "") {}
 		virtual std::string receive() { return ""; }
 		virtual void send(const std::string& data) {}
 		virtual void closeConnection() {}
