2badad8df1791525d10fd0dfdad337f91fdf3722
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-08-01 21:43:18 -0400

    Allow socket clients to set the line delimiter; default the delimiter to \r\n

--- src/modtypes/socket.h
+++ src/modtypes/socket.h
@@ -3,8 +3,10 @@
 
 class Socket {
 	public:
+		Socket() : delimiter("\r\n") {}
 		virtual ~Socket() {}
 		virtual unsigned int apiVersion() = 0;
+		void setDelimiter(const std::string& delim) { delimiter = delim; }
 		virtual void connectServer(const std::string& server, const std::string& port, const std::string& bindAddr = "") {}
 		virtual std::string receive() { return ""; }
 		virtual void send(const std::string& data) {}
@@ -12,4 +14,5 @@ class Socket {
 		bool isConnected() { return connected; }
 	protected:
 		bool connected;
+		std::string delimiter;
 };
