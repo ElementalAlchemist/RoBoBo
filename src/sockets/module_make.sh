60d06ffc567fab7a1d444b62b3dd68dd80e8ddbe
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-08-04 18:18:22 -0400

    Fix building protocol and socket modules

--- src/sockets/module_make.sh
+++ src/sockets/module_make.sh
@@ -1,5 +1,5 @@
 cd src/sockets # This script is called from the root makefile
 shopt -s nullglob
 for source in *.cpp; do
-	printf "`$CC $GENDEPS $source $GENDEPSTARGET ../../modules/${source:0:${#source}-4}.so`\n\t@echo \"\e[1;32mBuild:\e[0m\t$source\"\n\t@$CC $CXXFLAGS $SOFLAGS $source $LDFLAGS `grep '^\s*// COMPILE_FLAGS ' $source | awk -F'[ ]' '{$1=$2=""; print $0}'` -c -o ../../modules/${source:0:${#source}-4}.so\n\n"
+	printf "`$CC $GENDEPS $source $GENDEPSTARGET ../../sockets/${source:0:${#source}-4}.so`\n\t@echo \"\e[1;32mBuild:\e[0m\t$source\"\n\t@$CC $CXXFLAGS $SOFLAGS $source `grep '^\s*// COMPILE_FLAGS ' $source | awk -F'[ ]' '{$1=$2=""; print $0}'` -c -o ../../sockets/${source:0:${#source}-4}.so\n\n"
 done
