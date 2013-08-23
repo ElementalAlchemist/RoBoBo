3a75cb0682da5f3a260ebf7dbd96352eb15aa81a
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-08-23 16:44:16 -0400

    Actually build the directories

--- module_make.sh
+++ module_make.sh
@@ -3,3 +3,6 @@ shopt -s nullglob
 for source in *.cpp; do
 	printf "`$CC $GENDEPS $source $GENDEPSTARGET ../../$1/${source:0:${#source}-4}.so`\n\t@echo \"\e[1;32mBuild:\e[0m\t$source\"\n\t@$CC $CXXFLAGS $SOFLAGS $source `grep '^\s*// COMPILE_FLAGS ' $source | awk -F'[ ]' '{$1=$2=""; print $0}'` -c -o ../../$1/${source:0:${#source}-4}.so\n\n"
 done
+for dir in */; do
+	printf "../../$1/${dir%?}:\n\t@echo \"\e[1;32mBuild:\e[0m\t$dir\"\n\t@make --no-print-directory -C $dir\n\n"
+done
