97bbd18260cf1f611e5896eeda2ef41366d3aec1
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-09-16 13:44:35 -0400

    Store supported CAP capabilities

--- src/protocols/client/protocol.h
+++ src/protocols/client/protocol.h
@@ -93,4 +93,6 @@ class Protocol : public ClientProtocol {
 		std::unordered_map<std::string, std::shared_ptr<Channel>> channels;
 		std::unordered_map<std::string, std::shared_ptr<Client>> clients;
 		std::unordered_map<std::string, std::string> nickToID;
+		
+		std::set<std::string> capabilities;
 };
