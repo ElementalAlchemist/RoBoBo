e0f70dc7f35fb7fedd12948bab490c5634e492e3
ElementalAlchemist <ElementAlchemist7@gmail.com>
2013-09-16 18:32:21 -0400

    Save an easy conversion back from modes to types

--- src/protocols/client/protocol.h
+++ src/protocols/client/protocol.h
@@ -106,6 +106,8 @@ class Protocol : public ClientProtocol {
 		
 		std::unordered_multimap<ModeType, std::string> serverChanModes;
 		std::unordered_multimap<ModeType, std::string> serverUserModes;
+		std::unordered_map<std::string, ModeType> chanModeType;
+		std::unordered_map<std::string, ModeType> userModeType;
 		
 		std::unordered_map<std::string, char> chanPrefixModeToSymbol;
 		std::unordered_map<char, std::string> chanPrefixSymbolToMode;
