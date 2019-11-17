ab3f49b0896ac5c063cb10271935f2ad1645d0d6
ElementalAlchemist <ElementAlchemist7@gmail.com>
2019-11-17 03:36:51 -0600

    Initial command line argument reading

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 mod config;
 
-pub fn run() {
-	
+pub fn run(config_file_name: &str, debug_level: u32) {
+	config::read_config(config_file_name); // TODO Use the config
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{App, Arg};
+
 fn main() {
-	robobo::run();
+	let option_values = App::new("RoBoBo IRC Bot")
+		.version("3.0.0-dev") // TODO Get the version from a more common location
+		.about("Customizable bot for IRC")
+		.arg(
+			Arg::with_name("debug")
+				.short("d")
+				.long("debug")
+				.multiple(true)
+				.help("Sets the debug level"),
+		)
+		.arg(
+			Arg::with_name("config")
+				.long("config")
+				.value_name("FILENAME")
+				.takes_value(true)
+				.help("Loads the configuration from the specified file instead of the default (robobo.conf)"),
+		)
+		.get_matches();
+
+	let config_file_name = option_values.value_of("config").unwrap_or("robobo.conf");
+	let debug_level = option_values.occurrences_of("debug");
+	let debug_level = if debug_level > u32::max_value().into() {
+		u32::max_value()
+	} else {
+		debug_level as u32
+	};
+
+	robobo::run(config_file_name, debug_level);
 }
