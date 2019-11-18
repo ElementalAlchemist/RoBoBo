120dda5fe7905513c02ae42f9750481b7fc40df8
ElementalAlchemist <ElementAlchemist7@gmail.com>
2019-11-17 20:57:33 -0600

    Set up for reading configuration

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,18 @@
 mod config;
+use std::error::Error;
 
 pub fn run(config_file_name: &str, debug_level: u32) {
-	config::read_config(config_file_name); // TODO Use the config
+	let config_data = match config::read_config(config_file_name) {
+		Ok(config) => config,
+		Err(error) => {
+			match error {
+				config::ConfigError::FileError(err) => eprintln!(
+					"An error occurred reading the configuration file: {}",
+					err.description()
+				),
+				config::ConfigError::ParseError(err) => eprintln!("{}", err),
+			}
+			return;
+		}
+	};
 }
