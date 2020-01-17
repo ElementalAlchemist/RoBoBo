86ec9b235c32f50c6d5ce604ba45aea4363d902b
ElementalAlchemist <ElementAlchemist7@gmail.com>
2020-01-16 22:09:17 -0600

    Add reading config options to log setup

--- src/logger.rs
+++ src/logger.rs
@@ -1,5 +1,7 @@
+use std::collections::HashMap;
 use std::fs;
 use std::io::Write;
+use std::num::ParseIntError;
 
 pub struct Logger {
 	log_file: Option<fs::File>,
@@ -7,7 +9,29 @@ pub struct Logger {
 }
 
 impl Logger {
-	pub fn new(log_file: Option<fs::File>, log_level: u32) -> Logger {
+	pub fn new(config: &HashMap<String, String>, debug_level: u32) -> Logger {
+		let mut log_file: Option<fs::File> = None;
+		if debug_level == 0 {
+			let mut log_file_name = "robobo.log";
+			if config.contains_key("file") {
+				log_file_name = &config["file"];
+			}
+			let log_open_result = fs::File::open(log_file_name);
+			if let Ok(file) = log_open_result {
+				log_file = Some(file);
+			} else {
+				eprintln!("Failed to open log file for writing; no log will be created");
+			}
+		}
+
+		let mut log_level: u32 = 0;
+		if config.contains_key("level") {
+			log_level = config["level"].parse().unwrap_or_else(|error: ParseIntError| {
+				eprintln!("Failed to read integer log level ({}); using log level 0", error);
+				0
+			});
+		}
+
 		Logger { log_file, log_level }
 	}
 
