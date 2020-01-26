47af9fe097930494d23eb742227cc5674d461010
ElementalAlchemist <ElementAlchemist7@gmail.com>
2020-01-26 13:39:29 -0600

    Add some basic IRC data

--- src/irc_data.rs
+++ src/irc_data.rs
@@ -1,3 +1,27 @@
-pub struct User {}
+use chrono::{DateTime, Utc};
+use std::collections::HashMap;
+use std::sync::Weak;
 
-pub struct Channel {}
+pub struct User {
+	id: String,
+	nick: String,
+	ident: String,
+	host: String,
+	gecos: String,
+	channels: Vec<Weak<Channel>>,
+}
+
+pub struct Channel {
+	name: String,
+	topic: String,
+	topic_setter: String,
+	topic_time: DateTime<Utc>,
+	users: Vec<ChannelUser>,
+	list_modes: HashMap<char, Vec<String>>,
+	modes: HashMap<char, Option<String>>,
+}
+
+pub struct ChannelUser {
+	user: Weak<User>,
+	rank: char,
+}
